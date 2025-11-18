67d4a5e5773f0df0d557f0b4ebd0903af2dedf56
Rikuo <xxrikuoxx1@gmail.com>
2025-11-18 22:15:51 +0900

    add cognito info to config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_Vyi6gMWgd', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '72i6kdu4goussadao87ej40qfm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_mSWV8FO4s', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '38b5qtprf7e34kqo8e9g2h27gt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
