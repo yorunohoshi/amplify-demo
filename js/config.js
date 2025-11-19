055a25f627e4f7c05adf6bee34c653f05f512791
Rikuo <xxrikuoxx1@gmail.com>
2025-11-19 23:52:14 +0900

    add invokeUrl

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_mSWV8FO4s', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '38b5qtprf7e34kqo8e9g2h27gt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2 lp
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://phwb9mfsvh.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
